c46d7247697955d2b79f26e5aa6fc11fd9cbdda3
hund <vlvethund@gmail.com>
2024-04-14 02:26:49 +0900

    sys.path.append

--- test/marketCap.py
+++ test/marketCap.py
@@ -1,3 +1,7 @@
+
+import sys
+sys.path.append('.')
+
 import json
 
 import requests
@@ -8,6 +12,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from store.marketcaphistocialdata import MarketCapHistoricalData
+
 
 tz = timezone('America/New_York')
 load_dotenv()
